7f44c533afad5b67dc0824d000e5908152a09a46
youme <mrsimplelife@naver.com>
2021-03-09 08:53:02 +0900

    Add ant design

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,7 +1,14 @@
+import { Button } from "antd";
 import "App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Button type="primary" onClick={() => console.log("hi")}>
+        button
+      </Button>
+    </div>
+  );
 }
 
 export default App;
