ad0ad2cf44ff202dac70c7df8bf925a975d4f2ff
youme <mrsimplelife@naver.com>
2021-03-09 11:12:52 +0900

    add action

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -2,21 +2,48 @@ import { Button } from "antd";
 import "App.css";
 import { Component } from "react";
 
+const action = {
+  init(initialValue) {
+    return { value: initialValue };
+  },
+  increase(prev) {
+    return { value: prev.value + 1 };
+  },
+  decrease(prev) {
+    return { value: prev.value - 1 };
+  },
+};
 class Counter extends Component {
-  state = {
-    value: this.props.initialValue,
-  };
+  constructor(props) {
+    super(props);
+    this.state = action.init(this.props.initialValue);
+  }
+  // state = action.init(this.props.initialValue);
+  // {
+  //   value: this.props.initialValue,
+  // };
   onClick = () => {
-    const { value } = this.state;
-    this.setState({ value: value + 1 });
+    // const { value } = this.state;
+    // this.setState({ value: value + 1 });
+    // this.setState({ value: value + 1 });
+    // this.setState({ value: value + 1 });
+    this.setState(({ value }) => ({ value: value + 1 }));
+    this.setState(({ value }) => ({ value: value + 1 }));
+    this.setState(({ value }) => ({ value: value + 1 }));
   };
   render() {
     const { value } = this.state;
     return (
       <>
         <div>{value}</div>
-        <Button type="primary" onClick={this.onClick}>
+        <Button type="primary" onClick={() => this.setState(action.decrease)}>
+          -1
+        </Button>
+        <Button type="primary" onClick={() => this.setState(action.increase)}>
           +1
+        </Button>
+        <Button type="primary" onClick={this.onClick}>
+          +3
         </Button>
       </>
     );
@@ -30,7 +57,7 @@ class Fruit extends Component {
         <h1>
           <ul>
             {fruits.map((fruit) => (
-              <li>{fruit}</li>
+              <li key={fruit}>{fruit}</li>
             ))}
           </ul>
         </h1>
