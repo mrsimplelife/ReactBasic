17e06fa16b1019237a73e99d0117949648846c5a
youme <mrsimplelife@naver.com>
2021-03-09 09:10:05 +0900

    Add Counter, Fruit

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,12 +1,51 @@
 import { Button } from "antd";
 import "App.css";
+import { Component } from "react";
 
+class Counter extends Component {
+  state = {
+    value: this.props.initialValue,
+  };
+  onClick = () => {
+    const { value } = this.state;
+    this.setState({ value: value + 1 });
+  };
+  render() {
+    const { value } = this.state;
+    return (
+      <>
+        <div>{value}</div>
+        <Button type="primary" onClick={this.onClick}>
+          +1
+        </Button>
+      </>
+    );
+  }
+}
+class Fruit extends Component {
+  render() {
+    const { fruits } = this.props;
+    return (
+      <>
+        <h1>
+          <ul>
+            {fruits.map((fruit) => (
+              <li>{fruit}</li>
+            ))}
+          </ul>
+        </h1>
+      </>
+    );
+  }
+}
 function App() {
+  const fruits = ["banana", "apple", "strawberry"];
   return (
     <div className="App">
-      <Button type="primary" onClick={() => console.log("hi")}>
-        button
-      </Button>
+      <Counter initialValue={0} />
+      <Counter initialValue={0} />
+      <Counter initialValue={0} />
+      <Fruit fruits={fruits} />
     </div>
   );
 }
